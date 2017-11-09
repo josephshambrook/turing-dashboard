5a05616a85b4538b4a081afb51d5e629c1ac7f0b
Joseph Shambrook <josephshambrook@googlemail.com>
2017-11-09 15:14:00 +0000

    Ability to add components to an object, that is then emitted to the render function

--- src/components/molecules/TilesContainer.jsx
+++ src/components/molecules/TilesContainer.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 
+// import Tiles here!
+import TubeStatus from 'react-tube-status';
+//
+
 class TilesContainer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      tiles: [<TubeStatus />]
+    }
+  }
+
+  componentWillMount() {
+    // attach keys to all components added to tiles object in state
+    this.setState(() => {
+      let key = 0;
+      const tilesWithKeys = this.state.tiles.map(tile => {
+        key++;
+        return React.cloneElement(
+          tile,
+          {key}
+        )
+      });
+
+      return { tiles: tilesWithKeys }
+    });
   }
 
   render() {
     return (
-      <div>TilesContainer</div>
+      <div>
+        {this.state.tiles.map(tile => tile)}
+      </div>
     );
   }
 }
